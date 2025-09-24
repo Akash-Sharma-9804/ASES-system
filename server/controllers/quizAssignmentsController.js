2d65ee5793befc7f21b750f866eef84bc715f4cc
Akash Sharma <akashsharma9804@gmail.com>
2025-09-24 10:03:47 +0530

    fixed db time issue and score percentage

--- server/controllers/quizAssignmentsController.js
+++ server/controllers/quizAssignmentsController.js
@@ -158,10 +158,20 @@ exports.endAssessment = async (req, res) => {
   if (is_correct) score++;
 }
 
+  // ✅ Used total assigned questions instead of total answered
+  const [assignedCountRows] = await db.query(
+    "SELECT COUNT(*) as totalAssigned FROM assigned_questions WHERE assignment_id = ? AND user_id = ?",
+    [assignment_id, user_id]
+  );
+  const totalAssigned = assignedCountRows[0]?.totalAssigned || 1;
 
-  const totalAnswered = answers.length || 1; // avoid divide by zero
-const percentage = (score / totalAnswered) * 100;
-    const status = percentage >= passing_score ? "passed" : "failed";
+  const percentage = (score / totalAssigned) * 100;
+  const status = percentage >= passing_score ? "passed" : "failed";
+
+
+//   const totalAnswered = answers.length || 1; // avoid divide by zero
+// const percentage = (score / totalAnswered) * 100;
+//     const status = percentage >= passing_score ? "passed" : "failed";
 
     // ✅ Update quiz_assignments
     await db.query(
